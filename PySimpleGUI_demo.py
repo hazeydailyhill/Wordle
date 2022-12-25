f6525442310daa1fe2f6223e62c647223e467ceb
hazeydailyhill <76240464+hazeydailyhill@users.noreply.github.com>
2022-12-25 03:20:06 -0800

    add demo for PySimpleGUI

---
+++ PySimpleGUI_demo.py
@@ -0,0 +1,37 @@
+import PySimpleGUI as sg
+
+sg.theme('HotDogStand')
+
+#ALL THE STUFF INSIDE THE WINDOW
+layout = [
+    [sg.Text('enter a word:'), sg.Text(size=(7, 1), key='-OUTPUT-',
+    background_color = 'black', visible = False)],
+    [sg.Input(key='-IN-')],
+    [sg.Button('OK'), sg.Button('EXIT'), sg.Text(size=(32, 2))]
+    ]
+
+#MAKING THE WINDOW
+window = sg.Window('TITLE!!', layout, font=('comicsans',14))
+
+#EVENT LOOP
+# - event = what a user does (entering a word)
+# - value = leters given, etc ('hello')
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'EXIT':
+        break
+    if event == 'OK':
+        window['-OUTPUT-'].update(visible = True)         #the black bar is now visible
+        window['-OUTPUT-'].update(values['-IN-'].upper()) #put the word given over the bar
+        
+window.close()
+
+
+#ALL THE COLORS YOU CAN USE FOR BACKGROUNDS!
+#https://github.com/PySimpleGUI/PySimpleGUI/blob/master/DemoPrograms/Demo_Color_Names_Smaller_List.py
+
+#ALL THE THEMES!
+#https://www.geeksforgeeks.org/themes-in-pysimplegui/
+
+#THE PYSIMPLEGUI COOKBOOK
+#https://pysimplegui.readthedocs.io/en/latest/cookbook/#the-demo-programs-are-also-recipes
